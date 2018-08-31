0a4f2bebc417744f881bc9f8b21dce84458f9b0a
Scott Smythe <smythe3000@gmail.com>
2018-08-31 14:12:54 -0700

    Added initial seed data
    Add initial seed data and enumerable degrees.

--- src/roster.cpp
+++ src/roster.cpp
@@ -8,6 +8,7 @@
 #include "roster.h"
 #include "degree.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -21,12 +22,14 @@ Roster::~Roster() {
 }
 
 int main() {
-	Degree degree = SECURITY;
-
-	cout << "Hello, World!" << endl;
-	cout << "My degree is: " << degree << endl;
-
-
+	const string studentData[] =
+	 {
+	 "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+	 "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+	 "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+	 "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+	 "A5,Scott,Smythe,ssmyth2@wgu.edu,23,8,30,45,SOFTWARE"
+	 };
 	return 0;
 }
 
